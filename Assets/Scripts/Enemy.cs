a663a469a011f1545237bfc1fd3bf39b7222da6e
denisToader <toader.denis2308@gmail.com>
2017-06-16 18:01:33 +0300

    Game files
    All the game files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.name == "Player") {
+			Destroy (col.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+
+	public void RestartLevel(){
+		Application.LoadLevel ("GameScene");
+	}
+}
